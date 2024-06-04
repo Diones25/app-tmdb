ef8ebe4603886addbdbee8e34a7407a7171c8416
Diones Pereira Alves <pereiradiones987@gmail.com>
2024-06-04 12:08:12 -0300

    atualizando card de pessoas do filme

--- src/components/CardPersonMovieDetail.tsx
+++ src/components/CardPersonMovieDetail.tsx
@@ -15,19 +15,19 @@ type Props = {
 const CardPersonMovieDetail = ({ id, poster_path, title, character }: Props) => {
   return (
     <>
-      <Card className="min-w-[200px] h-[394px] rounded-sm hover:drop-shadow-2xl cursor-pointer">
+      <Card className="min-w-[140px] h-[265px] rounded-sm hover:drop-shadow-2xl cursor-pointer">
         <div>
           <img
-            className='w-full object-cover rounded-t-md'
+            className='w-[140px] h-[175px] object-cover rounded-t-md'
             src={poster_path}
             alt=""
           />
         </div>
         <CardFooter className="flex flex-col mt-5">
-          <div className="self-start">
+          <div className="self-start ml-[-13px] mt-[-10px]">
+            <h2 className=" font-bold text-sm hover:text-[#53B4E4]">{title}</h2>
+            <p className="text-sm">{ character }</p>
           </div>
-          <h2 className="self-start font-bold hover:text-[#53B4E4]">{title}</h2>
-          <p className="self-start">{ character }</p>
         </CardFooter>
       </Card>
     </>
